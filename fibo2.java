4e508db2abbbf8523ad1b07c1a19aadc7572f9e7
pravesh009 <pravesh.jain968@gmail.com>
2017-03-15 23:21:49 +0530

    Add files via upload
    Partial sum of Fibonacci

--- fibo2.java
+++ fibo2.java
@@ -4,25 +4,57 @@ import java.util.*;
 public class fibo2{
 		public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		long n=sc.nextLong();
-		long s=0,l=10;
-		long a=0,b=1,c=0;
-		long d=n%300;
-		if(n==0)
-				System.out.println("0");
-			
-		else if(n==1)
+		BigInteger m=sc.nextBigInteger();
+		BigInteger n=sc.nextBigInteger();
+		BigInteger s=BigInteger.valueOf(0);
+		BigInteger l=BigInteger.valueOf(10);
+		BigInteger a=BigInteger.valueOf(0);
+		BigInteger b=BigInteger.valueOf(1);
+		BigInteger c=BigInteger.valueOf(0);
+		BigInteger x=BigInteger.valueOf(1);
+		BigInteger y=BigInteger.valueOf(2);
+		BigInteger z=BigInteger.valueOf(300);
+		m=m.mod(z);
+		n=n.mod(z);
+		if(m.equals(s)&&n.equals(s))
+			{System.out.println("0");}
+
+		else if((m.equals(s)&&n.equals(x))||m.equals(x)&&n.equals(x))
 				System.out.println("1");
+			
+		
 		else
 		{
-			for(int i=2;i<=d;i++)
-			{
-				c=((a%l)+(b%l))%l;
-				s=(s+c)%l;
+			if(m.equals(s)||m.equals(x))
+				{
+					m=y;
+					s=x;
+			}
+			
+				
+			for (BigInteger i = BigInteger.valueOf(2);
+                i.compareTo(m) <0;
+                i = i.add(BigInteger.ONE)) {
+				c=a.add(b);
+				a=b;
+				b=c;
+	
+			}
+			//System.out.println(a+" "+b+" "+c);
+			for (BigInteger i=m;
+                i.compareTo(n) <= 0;
+                i = i.add(BigInteger.ONE)) {
+			
+				c=((a.mod(l)).add(b.mod(l))).mod(l);
+				//System.out.println(c);
+				s=(s.add(c)).mod(l);
+				//System.out.println(s);
+				
 				a=b;
 				b=c;
 			}
-			System.out.print((s+1)%l);
+			System.out.println(s);
+			
 		}
 		
 		}
